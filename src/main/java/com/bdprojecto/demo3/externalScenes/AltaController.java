c66043b458fc4f3135457598b0da98f3dda43a86
NexWan <l21051403@saltillo.tecnm.mx>
2023-04-11 11:19:04 -0600

    Forgot to configure the teachers part, now it's working for both users (student/teacher)

--- src/main/java/com/bdprojecto/demo3/externalScenes/AltaController.java
+++ src/main/java/com/bdprojecto/demo3/externalScenes/AltaController.java
@@ -36,77 +36,67 @@ public class AltaController implements Initializable {
             nombreAlumno.setCellValueFactory(new PropertyValueFactory<Alumno,String >("nomAlumno"));
             matriculaAlumno.setCellValueFactory(new PropertyValueFactory<Alumno,String>("matricula"));
             LinkedList<String> alumnosPendiente = new LinkedList<>(), profesoresPendiente = new LinkedList<>();
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","escuela","escuela");
-            Statement st = conn.createStatement();
-            String queryP = """
-                    SELECT nombre, matriculaP
-                    FROM escuela.profesores
-                    WHERE profesores.matriculaP NOT IN
-                    (SELECT u.idUsers
-                    FROM usuarios.users u JOIN Escuela.profesores p
-                    ON u.idUsers = p.matriculaP
-                    )""";
-            ResultSet rs = st.executeQuery(queryP);
-            while(rs.next()){
-                Profesor p = new Profesor(rs.getString("MATRICULAP"),rs.getString("NOMBRE"));
-                tabProf.getItems().add(p);
-            }
-            String queryA = """
-                    SELECT nombre, matriculaAl
-                    FROM escuela.alumnos\s
-                    WHERE alumnos.matriculaAl NOT IN
-                    (SELECT u.idUsers
-                    FROM usuarios.users u JOIN Escuela.alumnos a
-                    ON u.idUsers = a.matriculaAl
-                    )
-                    """;
-            rs = st.executeQuery(queryA);
-            while(rs.next()){
-                Alumno a = new Alumno(rs.getString("NOMBRE"),rs.getString("MATRICULAAL"));
-                tabAlumno.getItems().add(a);
-            }
+            dbConnection();
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void darAlta(ActionEvent actionEvent) {
-        String[] options = {"Si","Cancelar"};
+    public void darAlta(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","escuela","escuela");
+        Statement st = conn.createStatement();
         if(tabAlumno.getSelectionModel().getSelectedItem() != null){
             Alumno selected = tabAlumno.getSelectionModel().getSelectedItem();
             System.out.println(tabAlumno.getSelectionModel().getSelectedItem());
             try{
-                int option = JOptionPane.showConfirmDialog(null,String.format("El siguiente usuario se dara de alta: %s con matricula: %s y rol de profesor",selected.getNomAlumno(),selected.getMatricula()));
+                int option = JOptionPane.showConfirmDialog(null,String.format("El siguiente usuario se dara de alta: %s con matricula: %s y rol de Alumno",selected.getNomAlumno(),selected.getMatricula()));
                 if(option == JOptionPane.OK_OPTION){
-                    Class.forName("oracle.jdbc.driver.OracleDriver");
-                    Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","escuela","escuela");
-                    Statement st = conn.createStatement();
                     String user = selected.getNomAlumno().substring(0,3) + selected.getMatricula();
                     String clave = selected.getNomAlumno().substring(0,0) + selected.getMatricula();
                     String id = selected.getMatricula();
                     String rol = "Alumno";
-                    String query = String.format("INSERT INTO usuarios.users VALUES(%s,'%s','%s','%s')",id,user,clave,rol);
-                    ResultSet rt = st.executeQuery(query);
-                    if(rt.next()){
-                        JOptionPane.showMessageDialog(null,String.format("El usuario: %s con clave: %s y matricula: %s ha sido insertado!",user,id,clave));
-                        st.execute("commit");
-                        updateDb();
-                    }else{
-                        JOptionPane.showMessageDialog(null,"Ha ocurrido un error!","ERROR!",JOptionPane.ERROR_MESSAGE);
-                    }
+                    insertTable(st, user, clave, id, rol);
                 }
             }catch (Exception e){
                 JOptionPane.showMessageDialog(null,"Una excepcion ha ocurrido!","Error",JOptionPane.ERROR_MESSAGE);
             }
         }else if(tabProf.getSelectionModel().getSelectedItem() != null) {
-            System.out.println(tabProf.getSelectionModel().getSelectedItem().toString());
+            Profesor selected = tabProf.getSelectionModel().getSelectedItem();
+            try{
+                int option = JOptionPane.showConfirmDialog(null,String.format("El siguiente usuario se dara de alta: %s con matricula: %s y rol de Alumno",selected.getNombre(),selected.getMatricula()));
+                if(option == JOptionPane.OK_OPTION){
+                    String user = selected.getNombre().substring(0,3) + selected.getMatricula();
+                    String clave = selected.getNombre().substring(0,0) + selected.getMatricula();
+                    String id = selected.getMatricula();
+                    String rol = "Profesor";
+                    insertTable(st, user, clave, id, rol);
+                }
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null,"Una excepcion ha ocurrido!","Error",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void insertTable(Statement st, String user, String clave, String id, String rol) throws SQLException, ClassNotFoundException {
+        String query = String.format("INSERT INTO usuarios.users VALUES(%s,'%s','%s','%s')",id,user,clave,rol);
+        ResultSet rt = st.executeQuery(query);
+        if(rt.next()){
+            JOptionPane.showMessageDialog(null,String.format("El usuario: %s con clave: %s y matricula: %s ha sido insertado!",user,id,clave));
+            st.execute("commit");
+            updateDb();
+        }else{
+            JOptionPane.showMessageDialog(null,"Ha ocurrido un error!","ERROR!",JOptionPane.ERROR_MESSAGE);
         }
     }
 
     public void updateDb() throws SQLException, ClassNotFoundException {
         tabAlumno.getItems().clear();
         tabProf.getItems().clear();
+        dbConnection();
+    }
+
+    private void dbConnection() throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","escuela","escuela");
         Statement st = conn.createStatement();
@@ -137,5 +127,9 @@ public class AltaController implements Initializable {
             Alumno a = new Alumno(rs.getString("NOMBRE"),rs.getString("MATRICULAAL"));
             tabAlumno.getItems().add(a);
         }
+    }
+
+    private void insertIntoTable(){
+
     }
 }
